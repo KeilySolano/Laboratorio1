d735b7759a9b12ae94d9b4a57b3b52bdf0e86a02
KeilySolano <62618755+KeilySolano@users.noreply.github.com>
2020-04-28 16:26:44 -0600

    Cambio4

--- 1Laboratorio/1Laboratorio/Program.cs
+++ 1Laboratorio/1Laboratorio/Program.cs
@@ -6,7 +6,7 @@ namespace _1Laboratorio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("LABORATORIOs1 ");
+            Console.WriteLine("LABORATORIOs:1 ");
 
         }
     }
