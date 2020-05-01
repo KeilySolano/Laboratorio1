6b4eac5cfad55a2fb199693113761887de5ac56f
KeilySolano <62618755+KeilySolano@users.noreply.github.com>
2020-05-01 17:51:33 -0600

    Cambio11

--- 1Laboratorio/1Laboratorio/Inventario.cs
+++ 1Laboratorio/1Laboratorio/Inventario.cs
@@ -7,16 +7,29 @@ namespace _1Laboratorio
 {
     class Inventario
     {
-        static string inventario = "Inventario.txt";
         static StreamReader Leer;
         static StreamWriter Escribir;
 
         public void GProducto()
         {
+            StreamWriter inventario = File.AppendText("Inventario.txt");
+            int cantidad;
+            Console.WriteLine("Ingrese la cantidad");
+            cantidad = int.Parse(Console.ReadLine());
+            inventario.WriteLine(cantidad);
             int precio;
+            Console.WriteLine("Ingrese el precio");
             precio = int.Parse(Console.ReadLine());
-            Escribir.WriteLine(precio);           
-            Escribir.Close();
+            inventario.WriteLine(precio);
+            string producto;
+            Console.WriteLine("Ingrese producto");
+            producto = Console.ReadLine();
+            inventario.WriteLine(producto);
+            int total=cantidad*precio;
+            Console.WriteLine("El total es:"+total);
+            inventario.WriteLine(total);
+            inventario.Close();
+            
         }
         
     }
